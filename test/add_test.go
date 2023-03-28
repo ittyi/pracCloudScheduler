ff952a0f780210e88b7b26f871c95fe83d7b5b3d
ittyi <vioiv.k4@gmail.com>
2023-03-28 22:46:39 +0900

    add: test t.helper()

---
+++ test/add_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func TestAdd(t *testing.T) {
+	AssertEquals(t, 10, Add(1, 1))
+	AssertEquals(t, 10, Add(1, 2))
+	AssertEquals(t, 10, Add(1, 3))
+}
+
+func AssertEquals(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Unexpected int\nexpected:%d, actual:%d", expected, actual)
+	}
+}
