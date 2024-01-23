c581af164a7687d2e4d6c11b41c68591b2219b03
ittyi <vioiv.k4@gmail.com>
2024-01-23 17:29:45 +0900

    🐛 エラーを一旦消す

--- test/addhelper_test.go
+++ test/addhelper_test.go
@@ -1,18 +1,18 @@
-package testi
+package main
 
 import "testing"
 
-func Add(x, y int) int {
+func AddHelper(x, y int) int {
 	return x + y
 }
 
-func TestAdd(t *testing.T) {
+func TestAddHelper(t *testing.T) {
 	AssertEquals(t, 10, Add(1, 1))
 	AssertEquals(t, 10, Add(1, 2))
 	AssertEquals(t, 10, Add(1, 3))
 }
 
-func AssertEquals(t *testing.T, expected, actual int) {
+func AssertEqualsHelper(t *testing.T, expected, actual int) {
 	t.Helper()
 	if expected != actual {
 		t.Errorf("Unexpected int\nexpected:%d, actual:%d", expected, actual)
--- test/callmod.go
+++ test/callmod.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"github.com/ittyi/pracGoGeneral/modtest"
-	"log"
-)
-
-func main(){
-	log.Printf("check")
-	log.Printf("testi.Add(1, 1)", modtest.hello())
-}
+// func main(){
+// 	log.Printf("check")
+// 	log.Printf("testi.Add(1, 1)", modtest.hello())
+// }
